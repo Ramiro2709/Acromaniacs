7f86e6cfe1df92503d3a10ec32f321ca398b176d
Ramiro2709 <vargasramiro99@gmail.com>
2019-03-08 00:16:54 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public sqlite: SQLite
   ) {
     this.initializeApp();
   }
@@ -21,6 +23,27 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      this.createDatabase();
+    });
+  }
+
+  private createDatabase(){
+    this.sqlite.create({
+      name: 'contactos_local.db',
+      location: 'default' // the location field is required
+    })
+    .then((db : SQLiteObject) => {
+      console.log("Base de datos: "+db);
+      /*
+      this.tasksService.setDatabase(db);
+      //this.tasksService.prueba_borrartabla(); ////  Borra contenido de tabla contactos_local
+      this.tasksService.createTable_contactos();
+      return this.tasksService.createTable_numeros();
+      */
+    })
+    .catch(error =>{
+      console.error(error);
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { File } from '@ionic-native/File/ngx';
+import { FileOpener } from '@ionic-native/file-opener/ngx';
+import { FileTransfer } from '@ionic-native/file-transfer/ngx';
+import { SQLite } from '@ionic-native/sqlite/ngx';
+import {SQLiteServiceService} from '../services/sqlite-service.service';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,7 +22,12 @@ import { AppComponent } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    File,
+    FileOpener,
+    FileTransfer,
+    SQLite,
+    SQLiteServiceService
   ],
   bootstrap: [AppComponent]
 })
